3a5fa7b1903d8b35243d3a7d3335b9714638e7b7
Loganrun <bigdog_41@hotmail.com>
2013-11-06 23:55:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikisite::Application.config.secret_key_base = '0e088e4bc20ce360337b63b433d4e860076bb97793f147d7cea3b4ead8a65a07785a1f5368632cb2b3566f9f7e9d1cd87402ea4acf8859c7a91d562957a42614'
